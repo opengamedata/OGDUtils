adb9b05fc6fa5c935a5af18df8af075f93be9862
Luke Swanson <lwswanson2@wisc.edu>
2022-06-10 08:35:28 -0500

    Work towards getting the script to run correctly.

--- sql_to_BQ.py
+++ sql_to_BQ.py
@@ -1,64 +1,104 @@
 from datetime import datetime
+from typing import List, Tuple
 
 from google.cloud import bigquery_storage_v1
 from google.cloud.bigquery_storage_v1 import types
 from google.cloud.bigquery_storage_v1 import writer
 from google.protobuf import descriptor_pb2
 
-from config.config import settings
-from interfaces.MySQLInterface import MySQLInterface, SQL
-
-mysql = MySQLInterface("AQUALAB", settings=settings)
-start_date = datetime.now() # TODO: get start of yesterday.
-end_date = datetime.now() # TODO: get end of yesterday
-if "MYSQL_CONFIG" in settings.keys() and mysql._db_cursor is not None and mysql.IsOpen():
-    db_name = settings["MYSQL_CONFIG"]["DB_NAME"]
-    table_name = settings["MYSQL_CONFIG"]["TABLE"]
-    rows = SQL.SELECT(cursor=mysql._db_cursor, db_name=db_name, table=table_name,
-                      filter=f"`server_time` BETWEEN {start_date} AND {end_date}")
-    if rows is not None and "BIGQUERY_CONFIG" in settings.keys():
-        # Create a write stream, based on Google's sample code.
-        write_client = bigquery_storage_v1.BigQueryWriteClient()
-        project_id = settings["BIGQUERY_CONFIG"]["AQUALAB"]["PROJECT_ID"]
-        dataset_id = settings["BIGQUERY_CONFIG"]["AQUALAB"]["DATASET_ID"]
-        table_id   = settings["BIGQUERY_CONFIG"]["TABLE_NAME"]
-        parent = write_client.table_path(project_id, dataset_id, table_id)
-        write_stream = types.WriteStream()
-        write_stream.type_ = types.WriteStream.Type.PENDING
-        write_stream = write_client.create_write_stream(
-            parent=parent, write_stream=write_stream
-        )
-        stream_name = write_stream.name
-        # Create a template with fields needed for the first request.
-        request_template = types.AppendRowsRequest()
-        # The initial request must contain the stream name.
-        request_template.write_stream = stream_name
-
-        # So that BigQuery knows how to parse the serialized_rows, generate a
-        # protocol buffer representation of your message descriptor.
-        proto_schema = types.ProtoSchema()
-        proto_descriptor = descriptor_pb2.DescriptorProto()
-        customer_record_pb2.CustomerRecord.DESCRIPTOR.CopyToProto(proto_descriptor)
-        proto_schema.proto_descriptor = proto_descriptor
-        proto_data = types.AppendRowsRequest.ProtoData()
-        proto_data.writer_schema = proto_schema
-        request_template.proto_rows = proto_data
-
-        # Some stream types support an unbounded number of requests. Construct an
-        # AppendRowsStream to send an arbitrary number of requests to a stream.
-        append_rows_stream = writer.AppendRowsStream(write_client, request_template)
+from data_transfers import OGDFormat_pb2
+from data_transfers import CustomerRecordSample_pb2
+from data_transfers.config import settings
+from opengamedata.interfaces.MySQLInterface import MySQLInterface, SQL
 
 
+def create_row_data_sample(row_num: int, name: str):
+    """_summary_
+
+    :param row_num: _description_
+    :type row_num: int
+    :param name: _description_
+    :type name: str
+    :return: _description_
+    :rtype: _type_
+    """
+    row = CustomerRecordSample_pb2.CustomerRecordSample()
+    row.row_num = row_num
+    row.customer_name = name
+    return row.SerializeToString()
 
 
-def append_rows_pending(project_id: str, dataset_id: str, table_id: str):
+def create_row_data(row_vals:Tuple) -> OGDFormat_pb2.CustomerRecord:
+    """_summary_
 
+    :param row_vals: _description_
+    :type row_vals: Tuple
+    :return: _description_
+    :rtype: CustomerRecord
+    """
+    row = OGDFormat_pb2.CustomerRecord()
+    row.id = row_vals[0]
+    row.app_id = row_vals[1]
+    row.user_id = row_vals[2]
+    row.user_data = row_vals[3] #TODO: fix
+    row.session_id = row_vals[4]
+    row.event_name = row_vals[5]
+    row.event_data = row_vals[6] #TODO: fix
+    row.game_state = row_vals[7] #TODO: fix
+    row.event_time = row_vals[8]
+    row.event_time_ms = row_vals[9]
+    row.time_offset = row_vals[10]
+    row.app_version = row_vals[11]
+    row.app_branch = row_vals[12]
+    row.log_version = row_vals[13]
+    row.http_user_agent = row_vals[14]
+    row.remote_addr = row_vals[15]
+    row.event_sequence_index = row_vals[16]
+    return row.SerializeToString()
 
+def append_rows_pending(project_id:str, dataset_id:str, table_id:str):
+    """_summary_
+
+    :param project_id: _description_
+    :type project_id: str
+    :param dataset_id: _description_
+    :type dataset_id: str
+    :param table_id: _description_
+    :type table_id: str
+    """
+    write_client = bigquery_storage_v1.BigQueryWriteClient()
+    parent = write_client.table_path(project_id, dataset_id, table_id)
+    write_stream = types.WriteStream()
+    write_stream.type_ = types.WriteStream.Type.PENDING
+    write_stream = write_client.create_write_stream(
+        parent=parent, write_stream=write_stream
+    )
+    stream_name = write_stream.name
+    # Create a template with fields needed for the first request.
+    request_template = types.AppendRowsRequest()
+    # The initial request must contain the stream name.
+    request_template.write_stream = stream_name
+
+    # So that BigQuery knows how to parse the serialized_rows, generate a
+    # protocol buffer representation of your message descriptor.
+    proto_schema = types.ProtoSchema()
+    proto_descriptor = descriptor_pb2.DescriptorProto()
+    CustomerRecordSample_pb2.CustomerRecordSample.DESCRIPTOR.CopyToProto(proto_descriptor)
+    proto_schema.proto_descriptor = proto_descriptor
+    proto_data = types.AppendRowsRequest.ProtoData()
+    proto_data.writer_schema = proto_schema
+    request_template.proto_rows = proto_data
+
+    # Some stream types support an unbounded number of requests. Construct an
+    # AppendRowsStream to send an arbitrary number of requests to a stream.
+    append_rows_stream = writer.AppendRowsStream(write_client, request_template)
     # Create a batch of row data by appending proto2 serialized bytes to the
     # serialized_rows repeated field.
     proto_rows = types.ProtoRows()
-    proto_rows.serialized_rows.append(create_row_data(1, "Alice"))
-    proto_rows.serialized_rows.append(create_row_data(2, "Bob"))
+    proto_rows.serialized_rows.append(create_row_data_sample(1, "Alice"))
+    proto_rows.serialized_rows.append(create_row_data_sample(2, "Bob"))
+
+
 
     # Set an offset to allow resuming this stream if the connection breaks.
     # Keep track of which requests the server has acknowledged and resume the
@@ -77,7 +117,7 @@ def append_rows_pending(project_id: str, dataset_id: str, table_id: str):
 
     # Send another batch.
     proto_rows = types.ProtoRows()
-    proto_rows.serialized_rows.append(create_row_data(3, "Charles"))
+    proto_rows.serialized_rows.append(create_row_data_sample(3, "Charles"))
 
     # Since this is the second request, you only need to include the row data.
     # The name of the stream and protocol buffers DESCRIPTOR is only needed in
@@ -109,3 +149,18 @@ def append_rows_pending(project_id: str, dataset_id: str, table_id: str):
     write_client.batch_commit_write_streams(batch_commit_write_streams_request)
 
     print(f"Writes to stream: '{write_stream.name}' have been committed.")
+
+mysql = MySQLInterface("AQUALAB", settings=settings)
+start_date = datetime.now() # TODO: get start of yesterday.
+end_date = datetime.now() # TODO: get end of yesterday
+if "MYSQL_CONFIG" in settings.keys() and mysql._db_cursor is not None and mysql.IsOpen():
+    db_name = settings["MYSQL_CONFIG"]["DB_NAME"]
+    table_name = settings["MYSQL_CONFIG"]["TABLE"]
+    rows = SQL.SELECT(cursor=mysql._db_cursor, db_name=db_name, table=table_name,
+                      filter=f"`server_time` BETWEEN {start_date} AND {end_date}")
+    if rows is not None and "BIGQUERY_CONFIG" in settings.keys():
+        # Create a write stream, based on Google's sample code.
+        _project_id = settings["BIGQUERY_CONFIG"]["AQUALAB"]["PROJECT_ID"]
+        _dataset_id = settings["BIGQUERY_CONFIG"]["AQUALAB"]["DATASET_ID"]
+        _table_id   = settings["BIGQUERY_CONFIG"]["TABLE_NAME"] if "TABLE_NAME" not in settings["BIGQUERY_CONFIG"]["AQUALAB"].keys() else settings["BIGQUERY_CONFIG"]["AQUALAB"]["TABLE_NAME"]
+        append_rows_pending(project_id=_project_id, dataset_id=_dataset_id, table_id=_table_id)
